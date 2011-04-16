f6fa9a00c7def60c6e9f1b908f1c833e5789a418
kuno <neokuno@gmail.com>
2011-04-16 09:29:32 +0800

    added help.cmk

--- bin/cmk.js
+++ bin/cmk.js
@@ -1,13 +1,21 @@
 #! /usr/bin/env node
 
-var util        = require('util'),
+var path        = require('path'),
+    util        = require('util'),
     consolemark = require('../index.js');
 
 var argv = process.argv;
 
-if (argv[2] === '-v') {  // version
+if (argv[2] === '-v' || argv[2] === '--version') {      // version
   util.puts(consolemark.version);
-} else if (argv[2] === '-h') {  // help
+} else if (argv[2] === '-h' || argv[2] === '--help') {  // help
+  var help = path.join(__dirname, '../doc/help.cmk');
+  consolemark.render(help, function(err, colorful) {
+      if (err) {throw err;}
+      colorful.forEach(function(c) {
+          util.print(c);
+      });
+  });
 } else {  // a path to file
   consolemark.render(argv[2], function(err, colorful) {
       if (err) {throw err;}
